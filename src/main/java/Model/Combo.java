ecfbe220e68290c413be70006ff7545dc1780adc
Dereck <dereckescalante@hotmail.com>
2019-09-03 16:06:41 -0600

    mvc

---
+++ src/main/java/Model/Combo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+import java.util.ArrayList;
+/**
+ *
+ * @author derec
+ */
+public class Combo {
+    private Plato plato;
+    private ArrayList<Bebida> bebida;
+    private ArrayList<Adicional> adicional;
+    
+    public Combo(Plato pla,ArrayList<Bebida> beb, ArrayList<Adicional> adi){
+        this.plato = pla;
+        this.bebida = beb;
+        this.adicional = adi;
+    }
+    public void setPlato(Plato p){
+        this.plato = p;
+    }
+    public void setBebida(ArrayList<Bebida> b){
+        this.bebida = b;
+    }
+    public void setAdicional(ArrayList<Adicional> a){
+        this.adicional = a;
+    }
+    public Plato getPlato(){
+        return this.plato;
+    }
+    public ArrayList<Bebida> getBebida(){
+        return this.bebida ;
+    }
+    public ArrayList<Adicional> getAdicional(){
+        return this.adicional;
+    }
+}
---
+++ src/main/java/Model/Plato.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+
+public class Plato extends Producto{
+    
+    
+    Plato(String cod,String nom,Float pre){
+        
+        super(cod,nom,pre);
+    }
+}
