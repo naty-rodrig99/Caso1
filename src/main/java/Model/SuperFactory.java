d867ae0af6c7bd04a674fadbadcca2427e7aea21
Dereck <dereckescalante@hotmail.com>
2019-09-04 23:40:24 -0600

    pruebas

---
+++ src/main/java/Model/SuperFactory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author derec
+ */
+public class SuperFactory implements IcomboFactoryMethod{
+    
+    
+    @Override
+    public void createCombo(String type){       //que retorne Combo
+        if(type.equals("este")){
+            //haga esto
+        }
+        if (type.equals("El otro")){
+            //hacer esto
+            //taki taki rumba
+        }
+        
+        return;
+    }
+}
